49f952d325021a61dd6ee836cf984e3c14d24c11
Unknown <pablo.saez@hotmail.com>
2018-05-11 14:25:00 -0300

    (has -> get) refactor, less calls to memory

--- src/Automata.js
+++ src/Automata.js
@@ -1,6 +1,6 @@
 import Stack from "stackjs";
 import Queue from "queue-fifo";
-import { forEach, merge, has } from "lodash";
+import { forEach, merge, get } from "lodash";
 
 export default class Automata {
   constructor(transitions = [], initialState = "", endState = undefined) {
@@ -11,7 +11,7 @@ export default class Automata {
     this.currentState = initialState;
     this.endState = endState;
     forEach(transitions, value => {
-      /* Iteramos sobre la lista de transiciones */
+      /* Iteramos sobre la lista de transiciones obtenidas de la interfaz */
       const { state, symbol, stack_from, state_to, stack_to } = value;
 
       merge(this.transitions, {
@@ -29,7 +29,7 @@ export default class Automata {
 
   replaceStack(stack_to) {
     stack_to = stack_to === "ε" ? "" : stack_to;
-    this.stack.pop(); /* NECESITO HACER VALIDACION SI EL STACK YA ESTA VACIO??? */
+    this.stack.pop();
     for (const c of this.reverse(stack_to)) this.stack.push(c);
   }
 
@@ -37,19 +37,20 @@ export default class Automata {
     for (const c of input) this.word.enqueue(c);
 
     while (
-      /* Si existe endState, la condicion del while es si currentState es distinto de endState */
-      /* Si no existe endState, la condicion del while es si se acabo la palabra */
-      !this.word.isEmpty() && this.endState ? this.currentState !== this.endState : !this.stack.isEmpty()
+      /* Primero se verifica si queda alguna subpalabra por analizar */
+      /* Despues, Si existe endState, la condicion del while es si currentState es distinto de endState */
+      /* Si no existe endState, la condicion del while es si se acabó el stack */
+      !this.word.isEmpty() && this.endState
+        ? this.currentState !== this.endState
+        : !this.stack.isEmpty()
     ) {
       const c = this.word.dequeue();
-      if (has(this.transitions, [this.currentState, this.stack.peek(), c])) {
-        const { state_to, stack_to } = this.transitions[this.currentState][
-          this.stack.peek()
-        ][c];
-        this.currentState = state_to;
-        this.replaceStack(stack_to);
-      } else return false;
-      /* Si no existe la transición que acepta la situacion, la palabra no es aceptada por el automata */
+      const { state_to, stack_to } =
+        get(this.transitions, [this.currentState, this.stack.peek(), c]) || {};
+      if (!state_to || !stack_to)
+        return false; /* Si la transicion no está definida, la palabra es rechazada */
+      this.currentState = state_to;
+      this.replaceStack(stack_to);
     }
 
     return this.endState
